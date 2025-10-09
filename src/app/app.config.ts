76a6dd5c434f2cb08d806dc5c103bb2a6e358955
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2025-10-09 14:05:04 -0300

    finalizando tela de login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,19 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+//import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideToastr(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch()),
+
   ]
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,20 +2,41 @@ import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInput } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   imports: [DefaultLoginLayoutComponent, ReactiveFormsModule, PrimaryInput],
+  providers: [
+    LoginService
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm!: FormGroup;
 
-  constructor() {
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastService: ToastrService
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required, Validators.minLength(6)])
     })
   }
+
+  submit() {
+    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+      next: () => this.toastService.success("Login successful!"),
+      error: () => this.toastService.error("Woops, something is wrong, try again later!")
+    })
+  }
+  navigate() {
+    this.router.navigate(["/signup"]);
+  }
+
 }
